d3828464f990488b9c4cdc8c6f8b09a50ecd7fae
naveenm0077 <naveenm0077@gmail.com>
2019-10-31 19:51:54 +0530

    modify game component
    Simplified the game logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -16,220 +16,26 @@ export class GameComponent implements OnInit {
   public msg: string = "";
   public buttonvalue = "🌞";
 
-  public valtl: string = "";
-  public valt: string = "";
-  public valtr: string = "";
-  public valcl: string = "";
-  public valc: string = "";
-  public valcr: string = "";
-  public valbl: string = "";
-  public valb: string = "";
-  public valbr: string = "";
-
   public xscore: number = 0;
   public tiescore: number = 0;
   public oscore: number = 0;
   public scoreflag: number = 0;
 
+  public board = ["", "", "", "", "", "", "", "", ""];
+
   fill(cell: number) {
-    if (cell == 1) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valtl == "") {
-            this.valtl = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valtl == "") {
-            this.valtl = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
+    if (this.turn == 1 && this.winflag == -1 && this.board[cell] == "") {
+      this.board[cell] = "X";
+      this.turn = this.turn + 1;
+      this.msg = "Player O's turn";
+      this.incrementcount();
+    } else if (this.winflag == -1 && this.board[cell] == "") {
+      this.board[cell] = "O";
+      this.turn = this.turn - 1;
+      this.msg = "Player X's turn";
+      this.incrementcount();
     }
-    if (cell == 2) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valt == "") {
-            this.valt = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valt == "") {
-            this.valt = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
-    if (cell == 3) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valtr == "") {
-            this.valtr = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valtr == "") {
-            this.valtr = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
-    if (cell == 4) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valcl == "") {
-            this.valcl = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valcl == "") {
-            this.valcl = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
-    if (cell == 5) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valc == "") {
-            this.valc = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valc == "") {
-            this.valc = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
-    if (cell == 6) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valcr == "") {
-            this.valcr = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valcr == "") {
-            this.valcr = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
-    if (cell == 7) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valbl == "") {
-            this.valbl = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valbl == "") {
-            this.valbl = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
-    if (cell == 8) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valb == "") {
-            this.valb = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valb == "") {
-            this.valb = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
-    if (cell == 9) {
-      if (this.turn == 1) {
-        if (this.winflag == -1) {
-          if (this.valbr == "") {
-            this.valbr = "X";
-            this.turn = this.turn + 1;
-            this.msg = "Player O's turn";
-            this.incrementcount();
-          }
-        }
-      } else {
-        if (this.winflag == -1) {
-          if (this.valbr == "") {
-            this.valbr = "O";
-            this.turn = this.turn - 1;
-            this.msg = "Player X's turn";
-            this.incrementcount();
-          }
-        }
-      }
-      this.checkwin();
-    }
+    this.checkwin();
   }
 
   incrementcount() {
@@ -240,14 +46,14 @@ export class GameComponent implements OnInit {
 
   checkwin() {
     if (
-      (this.valtl == "X" && this.valt == "X" && this.valtr == "X") ||
-      (this.valcl == "X" && this.valc == "X" && this.valcr == "X") ||
-      (this.valbl == "X" && this.valb == "X" && this.valbr == "X") ||
-      (this.valtl == "X" && this.valcl == "X" && this.valbl == "X") ||
-      (this.valt == "X" && this.valc == "X" && this.valb == "X") ||
-      (this.valtr == "X" && this.valcr == "X" && this.valbr == "X") ||
-      (this.valtl == "X" && this.valc == "X" && this.valbr == "X") ||
-      (this.valtr == "X" && this.valc == "X" && this.valbl == "X")
+      (this.board[0] == "X" && this.board[1] == "X" && this.board[2] == "X") ||
+      (this.board[3] == "X" && this.board[4] == "X" && this.board[5] == "X") ||
+      (this.board[6] == "X" && this.board[7] == "X" && this.board[8] == "X") ||
+      (this.board[0] == "X" && this.board[3] == "X" && this.board[6] == "X") ||
+      (this.board[1] == "X" && this.board[4] == "X" && this.board[7] == "X") ||
+      (this.board[2] == "X" && this.board[5] == "X" && this.board[8] == "X") ||
+      (this.board[0] == "X" && this.board[4] == "X" && this.board[8] == "X") ||
+      (this.board[2] == "X" && this.board[4] == "X" && this.board[6] == "X")
     ) {
       this.msg = "Player X wins";
       this.winflag = 1;
@@ -259,14 +65,14 @@ export class GameComponent implements OnInit {
       this.scoreflag = this.scoreflag + 1;
       if (this.scoreflag == 1) this.updatescore();
     } else if (
-      (this.valtl == "O" && this.valt == "O" && this.valtr == "O") ||
-      (this.valcl == "O" && this.valc == "O" && this.valcr == "O") ||
-      (this.valbl == "O" && this.valb == "O" && this.valbr == "O") ||
-      (this.valtl == "O" && this.valcl == "O" && this.valbl == "O") ||
-      (this.valt == "O" && this.valc == "O" && this.valb == "O") ||
-      (this.valtr == "O" && this.valcr == "O" && this.valbr == "O") ||
-      (this.valtl == "O" && this.valc == "O" && this.valbr == "O") ||
-      (this.valtr == "O" && this.valc == "O" && this.valbl == "O")
+      (this.board[0] == "O" && this.board[1] == "O" && this.board[2] == "O") ||
+      (this.board[3] == "O" && this.board[4] == "O" && this.board[5] == "O") ||
+      (this.board[6] == "O" && this.board[7] == "O" && this.board[8] == "O") ||
+      (this.board[0] == "O" && this.board[3] == "O" && this.board[6] == "O") ||
+      (this.board[1] == "O" && this.board[4] == "O" && this.board[7] == "O") ||
+      (this.board[2] == "O" && this.board[5] == "O" && this.board[8] == "O") ||
+      (this.board[0] == "O" && this.board[4] == "O" && this.board[8] == "O") ||
+      (this.board[2] == "O" && this.board[4] == "O" && this.board[6] == "O")
     ) {
       this.msg = "Player O wins";
       this.winflag = 2;
@@ -276,9 +82,8 @@ export class GameComponent implements OnInit {
   }
 
   updatescore() {
-    if (this.winflag == 1) {
-      this.xscore = this.xscore + 1;
-    } else if (this.winflag == 2) this.oscore = this.oscore + 1;
+    if (this.winflag == 1) this.xscore = this.xscore + 1;
+    else if (this.winflag == 2) this.oscore = this.oscore + 1;
     else if (this.winflag == 0) this.tiescore = this.tiescore + 1;
     else return;
   }
@@ -287,15 +92,8 @@ export class GameComponent implements OnInit {
     this.turn = 1;
     this.msg = "";
     this.count = 0;
-    this.valtl = "";
-    this.valt = "";
-    this.valtr = "";
-    this.valcl = "";
-    this.valc = "";
-    this.valcr = "";
-    this.valbl = "";
-    this.valb = "";
-    this.valbr = "";
+    this.board[0] = this.board[1] = this.board[2] = this.board[3] = this.board[4] = this.board[5] = this.board[6] = this.board[7] = this.board[8] =
+      "";
     this.winflag = -1;
     this.scoreflag = 0;
   }
